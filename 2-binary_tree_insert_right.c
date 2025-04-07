d94be7acf970208f9a851979177f2eee294ec98d
Genia888 <khalepo@gmail.com>
2025-04-07 15:01:19 +0200

    create 2 binary file whit the code inside

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_insert_right - function that inserts a noode
+ * as the right child of another node
+ * @parent: parent of the node
+ * value: value to the store on the new node
+ * Return: pointer to the created node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(NULL, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
+	}
+	parent->right = new_node;
+	new_node->parent = parent;
+	return (new_node);
+}
