fc66057267d5d072c751614707441c4ffc7ad1d0
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-07 17:41:40 +0200

    fix bug func null

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "binary_trees.h"
 
 /**
@@ -7,12 +8,12 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left != NULL)
-			binary_tree_postorder(tree->left, print_num);
+			binary_tree_postorder(tree->left, func);
 		if (tree->right != NULL)
-			binary_tree_postorder(tree->right, print_num);
+			binary_tree_postorder(tree->right, func);
 		if (func != NULL)
 			func(tree->n);
 	}
