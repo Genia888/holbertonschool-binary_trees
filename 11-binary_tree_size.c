7cda7369f661f50c2316565ed0390a4a1acdff4b
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-08 14:05:28 +0200

    betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,18 +9,18 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t hleft = 0;
-        size_t hright = 0;
-        size_t zero = 0;
+	size_t hleft = 0;
+	size_t hright = 0;
+	size_t zero = 0;
 
-        if (tree != NULL)
-        {
-                if (tree->left != NULL)
-                        hleft = binary_tree_size(tree->left);
-                if (tree->right != NULL)
-                        hright = binary_tree_size(tree->right);
-                return (1 + hleft + hright);
-        }
-        return (zero);
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			hleft = binary_tree_size(tree->left);
+		if (tree->right != NULL)
+			hright = binary_tree_size(tree->right);
+		return (1 + hleft + hright);
+	}
+	return (zero);
 
 }
